f55effd76f312329989124c166e624ef186ad031
Aliaksandr 'Axekash' Kuchutkov <kashim007@mail.ru>
2022-09-02 23:57:44 +0300

    feat: add new function

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
+function createDreamTeam(members) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let teamName = '';
+
+  for(let i = 0; i < members.length; i++) {
+    
+  }
 }
 
 module.exports = {
